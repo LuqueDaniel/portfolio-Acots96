4c480dec5aa7ac8e49533228b42cc861bd9827d8
Christian Viñolo <christianvf.adev@gmail.com>
2025-04-15 17:04:55 +0200

    Add projects data in landing page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,79 +2,140 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FaGithub, FaGitlab, FaLinkedin, FaEnvelope } from 'react-icons/fa'; // Importar los íconos
 import aleixImage from '../assets/aleix.png'; // Asegúrate de que la ruta sea correcta
+import CityZombiesImage from '../assets/CityZombies3.png';
+import LibraryOfBabelVideo from '../assets/TheLibraryofBabelReleaseTrailer.mp4';
+import Maze3DVideo from '../assets/Raylib_Maze3D.mp4';
+import ParatrooperVideo from '../assets/Raylib_Paratrooper.mp4';
+import RPrototyperVideo from '../assets/RPrototyper_Demo01_SHORT.mp4';
+import SciFiGameVideo from '../assets/UE_Scifi002_SHORT.mp4';
 import ProjectCard from './ProjectCard'; // Importar el componente de tarjeta de proyecto
 
-
 const Portfolio = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const [scrollEnabled, setScrollEnabled] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
 
-  // Datos de los proyectos
+  // Datos de los proyectos combinados
   const projects = [
     {
-      name: 'The Library of Babel',
-      year: 2024,
-      teamSize: 4,
-      duration: '6 Years',
-      tool: 'Unity3D (C#)',
-      description: 'The library of Babel is a tale revolving around a dark futuristic world, unfolding through platformer elements.',
-      videoId: 'Dw97lkxlWyU',
-      website: 'https://tanukigamestudio.com/our-games/the-library-of-babel'
-    },
-    {
-      name: 'Kluest',
-      year: 2024,
-      teamSize: 14,
-      duration: '3 Years',
-      tool: 'Unity3D (C#)',
-      description: 'Kluest is a mobile AR Metaverse where anyone can create experiences without thechnical knowledge.',
-      videoId: 'DyAsX5BNrBw',
-      website: 'https://kluest.com'
+      name: 'The Library of Babel (Unity)',
+      videoId: LibraryOfBabelVideo,
+      website: 'https://tanukigamestudio.com/our-games/the-library-of-babel',
+      data: {
+        Title: 'The Library of Babel (Unity)',
+        ShortDescription: 'A 2D stealth platformer graphic adventure published in 2023 by TanukiGameStudio and NeonDoctrine for PC and consoles.',
+        Description: 'The Library of Babel is a game by TanukiGameStudio (@tanuki_gs), a little studio founded by a group of friends. We started the game in 2020 and we released in 2023 for several platforms such as Steam, EpicGames, PS4, Xbox, Switch and iPhone, thanks to PlayStationTalents-GamesCamp who helped us during the development and NeonDoctrine, an amazing publisher who helped us to complete the game and publish it in more than 10 languages, including Chinese and Japanese. As we were a really small team, my tasks were mainly to implement the AI of the characters (FSM and Behaviour trees) and various systems such as the main character\'s actions, coins system, part of the UI, etc.',
+        TechnicalAspects: [
+          'AI implementation using FSM and Behaviour trees.',
+          'General systems development for gameplay mechanics.',
+          'Multi-platform release including Steam, EpicGames, PS4, Xbox, Switch, and iPhone.'
+        ],
+        URLs: {
+          Web: 'https://tanukigamestudio.com/our-games/the-library-of-babel',
+        }
+      }
     },
     {
       name: 'Maze3D',
       year: 2025,
-      teamSize: 1,
-      duration: '3 Weeks',
-      tool: 'Raylib (C++)',
-      description: 'This is a maze 3D game, made with the graphic library Raylib, shaders and modern C++.',
-      videoId: 'tSi3oM71Zi8',
-      website: 'https://acots96.itch.io/maze3d'
-    },
-    {
-      name: 'Pacman',
-      year: 2025,
-      teamSize: 1,
-      duration: '4 Days',
-      tool: 'Raylib (C++)',
-      description: 'This is a mini version of the Pacman game, made with the graphic library Raylib and modern C++.',
-      videoId: 'T9nZaKyVOsg',
-      website: 'https://acots96.itch.io/pacman'
+      videoId: Maze3DVideo,
+      website: 'https://acots96.itch.io/maze3d',
+      data: {
+        Title: 'Maze3D (Raylib/C++)',
+        ShortDescription: 'A little game about collecting items and solving mazes made with the graphic library Raylib, shaders for effects and C++.',
+        Description: 'This is a simple 3D project made with Raylib and C++ during my master degree where you need to collect all the objects randomly placed in the maze to open the exit.',
+        TechnicalAspects: [
+          'Mazes loaded dynamically as images checking the color of the pixel.',
+          '3D math operations (vectors, quaternions, matrices) for object movement and rotation.',
+          'Implemented simple shaders for visual effects.'
+        ],
+        URLs: {
+          Itch: 'https://acots96.itch.io/maze3d',
+          Gitlab: 'https://gitlab.com/Acots96/raylib_maze',
+        }
+      }
     },
     {
       name: 'Paratrooper',
       year: 2025,
-      teamSize: 1,
-      duration: '2 Days',
-      tool: 'Raylib (C++)',
-      description: 'This is a replica of the paratrooper game, made with the graphic library Raylib and modern C++.',
-      videoId: 'Qe5JD7qyNG4',
-      website: 'https://acots96.itch.io/paratrooper'
+      videoId: ParatrooperVideo,
+      website: 'https://acots96.itch.io/paratrooper',
+      data: {
+        Title: 'Paratrooper (Raylib/C++)',
+        ShortDescription: 'A tiny replica of the Paratrooper game made with Raylib and C++. I developed my own tiny engine architecture to manage all the objects.',
+        Description: 'This is a simple replica of the Paratrooper arcade game made with Raylib and C++ during my master degree.',
+        TechnicalAspects: [
+          'Implemented a tiny architecture imitating how engines work.',
+          'Class Actor for all objects and a class Engine to manage their lifetime and interactions.'
+        ],
+        URLs: {
+          Itch: 'https://acots96.itch.io/paratrooper',
+          Gitlab: 'https://gitlab.com/Acots96/raylib_paratrooper',
+        }
+      }
     },
     {
-      name: 'CityZombies',
+      name: 'CityZombies (Unity)',
       year: 2021,
-      teamSize: 1,
-      duration: '6 Months',
-      tool: 'Unity3D (C#)',
-      description: 'Use vehicles and fire weapons (or something else hidden in the map) to kill as many zombies as you can.',
-      videoId: 'gFnH9yPQsss',
-      website: 'https://acots96.itch.io/cityzombies'
+      imageUrl: CityZombiesImage,
+      website: 'https://acots96.itch.io/cityzombies',
+      data: {
+        Title: 'CityZombies (Unity)',
+        ShortDescription: 'A short zombies 3rd person action game I made during my master degree.',
+        Description: 'This is a tiny zombies game made with Unity and C# during my master degree where you have to kill as many zombies as you can and try to find hidden objects in the map.',
+        TechnicalAspects: [
+          'Modular system for weapons with different behaviors based on weapon type.',
+          'Vehicles system with nodes for autonomous movement.'
+        ],
+        URLs: {
+          Itch: 'https://acots96.itch.io/cityzombies',
+          Gitlab: 'https://gitlab.com/Acots96/practicafinal3d',
+        }
+      }
+    }
+  ];
+
+  // Nuevos proyectos en desarrollo
+  const underDevelopmentProjects = [
+    {
+      name: 'RPrototyper (Raylib/C++)',
+      videoId: RPrototyperVideo,
+      website: 'https://github.com/Acots96/RPrototyper',
+      data: {
+        Title: 'RPrototyper (Raylib/C++)',
+        ShortDescription: 'A simple scene editor to create your own hierarchy of 3D objects with its components and export it as a JSON ready to load in your game.',
+        Description: 'This is a 3D scene editor made with Raylib and modern C++ aimed to load 3D objects, place them in a hierarchy of objects (parent, childs, grandchilds, etc.) and export all together as a JSON ready to be loaded in your Raylib game or wherever you want. The idea came out when I was trying to make a 3D game with Raylib.',
+        TechnicalAspects: [
+          'MVC to implement separately the UI and the objects in the map.',
+          '3D math (vectors, quaternions, matrices, etc.) to allow the translation, rotation and scaling of the objects placed in the map, and a tree structure to allow objects to be placed as childs of other objects.',
+          'Check the GitHub repository for more details about the implementation.'
+        ],
+        URLs: {
+          GitHub: 'https://github.com/Acots96/RPrototyper',
+        }
+      }
     },
+    {
+      name: 'Sci-fi game (Unreal)',
+      videoId: SciFiGameVideo,
+      website: 'https://gitlab.com/Acots96/projectutapau',
+      data: {
+        Title: 'Sci-fi game (Unreal)',
+        ShortDescription: 'A 3rd person action-adventure RPG game in early stages.',
+        Description: 'I have developed my own modular systems of abilities and movements using UObjects and DataAssets so I can implement all the characters\' different actions and movements such as Link\'s parabella or snowboard. This is a project I am developing to implement some of the main mechanics of my favourite genre: action-adventure RPG games, which include a modular system for abilities and movements, and inventory system with different items, among others.',
+        TechnicalAspects: [
+          'DataAssets instead of ActorComponents for the abilities, instead it uses one single ActorComponent "AbilitiesManager" to manage all the abilities and movements.',
+          'I\'ve used ControlRig and just a couple of animations to implement Link\'s parabella movement and snowboard movement.',
+          'Check the GitLab repository for more details about the implementation.'
+        ],
+        URLs: {
+          GitLab: 'https://gitlab.com/Acots96/projectutapau',
+        }
+      }
+    }
   ];
 
   useEffect(() => {
@@ -85,14 +146,21 @@ const Portfolio = () => {
       document.body.style.overflow = 'auto'; // Activar el scroll
     }, 1000);
 
+    // Aplicar clase de modo oscuro al body
+    if (darkMode) {
+      document.body.classList.add('bg-gray-900', 'text-white');
+    } else {
+      document.body.classList.remove('bg-gray-900', 'text-white');
+    }
+
     return () => clearTimeout(timer); // Limpiar el timer al desmontar
-  }, []);
+  }, [darkMode]);
 
   return (
     <div className={`relative min-h-screen flex flex-col ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-900'} transition duration-500`}>
       {/* Contenedor centrado para el contenido con margen superior */}
       <motion.div
-        className="flex flex-col items-center mb-8 mt-20" // Añadir mt-20 para margen superior
+        className={`flex flex-col items-center mb-8 mt-20 px-4 max-w-4xl mx-auto`} // Añadir max-w y centrar
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1 }}
@@ -104,30 +172,39 @@ const Portfolio = () => {
           <img src={aleixImage} alt="Aleix Cots" className="object-cover w-full h-full" />
         </motion.div>
         <h1 className="text-3xl font-bold mt-4">Aleix Cots Molina</h1>
-        <h2 className="text-xl font-semibold mt-2">Videogame Developer</h2>
+        <h2 className="text-xl font-semibold mt-2">Game Programmer</h2>
+
+        {/* Información personal en un contenedor */}
+        <div className={`mt-4 p-4 rounded ${darkMode ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-900'} text-center`}>
+          <p>
+            Hi! I am Aleix, a game programmer from Barcelona, Spain, focused on gameplay, systems, and making games feel amazing to play. Mainly working with Unreal Engine and C++, and always looking for ways to push mechanics, feedback, and fun.
+            Currently being part of CreekGames as an Unreal dev and working as a Raylib/C++ teacher in a videogames programming degree (CITM – UPC).
+            Feel free to contact me for anything you want!
+          </p>
+        </div>
 
         {/* Íconos clicables */}
         <div className="flex space-x-4 mt-4">
           <a href="https://gitlab.com/Acots96" target="_blank" rel="noopener noreferrer">
-            <FaGitlab className="text-2xl transition duration-500 hover:text-orange-500" /> {/* Color de GitLab: Naranja */}
+            <FaGitlab className="text-2xl transition duration-500 hover:text-orange-500" />
           </a>
           <a href="https://github.com/Acots96" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="text-2xl transition duration-500 hover:text-purple-600" /> {/* Color de GitHub: Morado */}
+            <FaGithub className="text-2xl transition duration-500 hover:text-purple-600" />
           </a>
           <a href="https://www.linkedin.com/in/aleix-cots-molina-8bb00a153" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="text-2xl transition duration-500 hover:text-blue-600" /> {/* Color de LinkedIn */}
+            <FaLinkedin className="text-2xl transition duration-500 hover:text-blue-600" />
           </a>
           <a href="mailto:acots96@gmail.com" target="_blank" rel="noopener noreferrer">
-            <FaEnvelope className="text-2xl transition duration-500 hover:text-gray-500" /> {/* Color de Email: Gris */}
+            <FaEnvelope className="text-2xl transition duration-500 hover:text-gray-500" />
           </a>
         </div>
 
         <a
-          href="https://drive.google.com/file/d/1rUcFg_f2DDWhUNTrpSz-E-wCONB9aNs6/view?usp=sharing" // Reemplaza esto con la URL de tu currículum
+          href="https://drive.google.com/file/d/1B83HPk9Cl2jqIxb_E19V75EqK3q3FGXF/view?usp=sharing" // Reemplaza esto con la URL de tu currículum
           target="_blank"
           rel="noopener noreferrer"
           className={`mt-4 px-6 py-2 transition duration-500 
-            ${darkMode ? 'bg-white text-gray-900 hover:bg-gray-200' : 'bg-black text-white hover:bg-gray-800'}`}
+            ${darkMode ? 'bg-white text-gray-900 hover:bg-gray-200 border border-gray-300' : 'bg-black text-white hover:bg-gray-800 border border-gray-600'}`}
         >
           Resume
         </a>
@@ -144,6 +221,22 @@ const Portfolio = () => {
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} darkMode={darkMode} />
           ))}
+
+          {/* Barra de separación y nueva sección "Under Development" */}
+          <div className="my-8">
+            <hr className={`border-t-2 ${darkMode ? 'border-gray-600' : 'border-gray-300'}`} />
+            <h2 className="text-2xl font-bold text-center mt-4">Under Development</h2>
+            <motion.div
+              className="flex flex-wrap justify-center mt-4"
+              initial={{ opacity: 0, y: 50 }} // Comienza desde abajo
+              animate={{ opacity: 1, y: 0 }} // Termina en su posición final
+              transition={{ duration: 0.5 }} // Duración de la animación
+            >
+              {underDevelopmentProjects.map((project, index) => (
+                <ProjectCard key={index} project={project} darkMode={darkMode} />
+              ))}
+            </motion.div>
+          </div>
         </motion.div>
       )}
     </div>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,30 +1,72 @@
 import React from 'react';
-import { FaUsers, FaClock, FaCode } from 'react-icons/fa';
+import { FaGlobe, FaItchIo, FaGitlab, FaGithub } from 'react-icons/fa';
 import '@justinribeiro/lite-youtube';
 
 const ProjectCard = ({ project, darkMode }) => {
+  // Construir la URL del video o la imagen
+  const videoUrl = project.videoId;
+  const imageUrl = project.imageUrl;
+
   return (
-    <div className={`rounded-none shadow-lg mb-4 ml-4 mr-4 w-80 transition duration-500 ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
-      <lite-youtube className="w-full h-48 object-cover" videoid={project.videoId} autoload autoplay></lite-youtube>
-      <div className={`flex justify-between items-center p-2 transition duration-500 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'} opacity-80`}>
-        <span className="flex items-center">
-          <FaUsers className="mr-1" />
-          {project.teamSize} people
-        </span>
-        <span className="flex items-center">
-          <FaClock className="mr-1" />
-          {project.duration}
-        </span>
-        <span className="flex items-center">
-          <FaCode className="mr-1" />
-          {project.tool}
-        </span>
+    <div className={`rounded-lg shadow-lg mb-4 mx-2 w-full sm:w-1/3 transition duration-500 ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+        {project.videoId ? (
+          <video className="absolute top-0 left-0 w-full h-full object-cover" autoPlay loop muted>
+            <source src={videoUrl} type="video/mp4" />
+            Tu navegador no soporta el elemento de video.
+          </video>
+        ) : (
+          <img src={imageUrl} alt={project.data.Title} className="absolute top-0 left-0 w-full h-full object-cover" />
+        )}
       </div>
-      <div className={`p-4 transition duration-500 ${darkMode ? 'bg-gray-800' : 'bg-gray-100'}`}>
-        <a href={project.website} target="_blank" rel="noopener noreferrer">
-          <h3 className="text-xl font-semibold">{project.name} ({project.year})</h3>
-        </a>
-        <p className="mt-2 text-gray-600">{project.description}</p>
+      <div className={`p-6 transition duration-500 ${darkMode ? 'bg-gray-800 text-white' : 'bg-gray-100 text-gray-900'}`}>
+        <div className="flex justify-between items-center">
+          <h3 className="text-2xl font-semibold">
+            <a href={project.website} target="_blank" rel="noopener noreferrer" className="hover:underline">
+              {project.data.Title}
+            </a>
+          </h3>
+          <div className="flex space-x-2">
+            {project.data.URLs.Web && (
+              <a href={project.data.URLs.Web} target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                <FaGlobe />
+              </a>
+            )}
+            {project.data.URLs.Itch && (
+              <a href={project.data.URLs.Itch} target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                <FaItchIo />
+              </a>
+            )}
+            {project.data.URLs.Gitlab && (
+              <a href={project.data.URLs.Gitlab} target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                <FaGitlab />
+              </a>
+            )}
+            {project.data.URLs.Github && (
+              <a href={project.data.URLs.Github} target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+                <FaGithub />
+              </a>
+            )}
+          </div>
+        </div>
+        
+        {/* ShortDescription con fondo gris claro */}
+        <div className={`mt-2 p-2 rounded ${darkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
+          <p>{project.data.ShortDescription}</p>
+        </div>
+
+        <h4 className="mt-4 font-semibold">Description:</h4>
+        <p className="mb-4">{project.data.Description}</p>
+
+        {/* Separador entre Descripción y Aspectos Técnicos */}
+        <hr className="my-4 border-gray-300" />
+
+        <h4 className="mt-4 font-semibold">Technical Aspects:</h4>
+        <ul className="list-disc list-inside mb-4">
+          {project.data.TechnicalAspects.map((aspect, index) => (
+            <li key={index}>{aspect}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
