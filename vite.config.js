4d316cfc5c11b8d7f3bc1a8c702a035864645540
Daniel Luque <danielluque14@gmail.com>
2025-04-16 20:07:09 +0200

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-Acots96/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
